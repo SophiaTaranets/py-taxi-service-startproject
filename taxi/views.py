2fd507bba164b9d7753e48a002214a5968e84067
SophiaTaranets <sony.taranets@gmail.com>
2023-04-05 22:04:18 +0300

    Додано файл .gitignore

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from taxi.models import Car, Driver, Manufacturer
+
+
+def index(request):
+    # обрпховуємо кількість екземплярів моделей
+    driver_number = Driver.objects.count()
+    car_number = Car.objects.count()
+    man_number = Manufacturer.objects.count()
+    context ={
+        "driver_number": driver_number,
+        "car_number": car_number,
+        "man_number": man_number
+    }
+    return render(request, "taxi/index.html", context=context)
